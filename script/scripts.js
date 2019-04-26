6cc6ac620cea9a0b49d58c146caeb7bb55302d0a
Regi Voda <vodaregi@gmail.com>
2019-04-26 16:02:50 +0200

    minor fixes again

--- script/scripts.js
+++ script/scripts.js
@@ -2,17 +2,28 @@
 const keysound = () =>{
   window.addEventListener('keydown', function(e) {
       const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
-      const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+      const key = document.querySelector(`kbd[data-key="${e.keyCode}"]`);
+      const icon = document.querySelector(`i[data-key="${e.keyCode}"]`)
       const ripple = document.querySelector(".rip");
       if (!audio) return;
       audio.currentTime = 0;
       audio.play();
       key.classList.add("playing");
       ripple.classList.add("lds-ripple");
+      icon.classList.add("playing_i");
         setTimeout(function(){
           ripple.classList.remove("lds-ripple");
         }, 10000);
-      key.classList.remove("playing");
     })
+  function removeTransition(e) {
+    if (e.propertyName !== 'transform') return;
+    e.target.classList.remove('playing');
+    e.target.classList.remove('playing_i');
+
+  }
+  const keys = Array.from(document.querySelectorAll('kbd'));
+  keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+  window.addEventListener('keydown', keysound);
+
 }
 keysound();
